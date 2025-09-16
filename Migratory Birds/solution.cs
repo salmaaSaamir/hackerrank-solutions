c5f0c96451fd6ab01fb402b4c4827b618be75510
SalmaaSaamir <salmasaamir554@gmail.com>
2025-09-16 18:37:08 +0300

    solve Migratory Birds and Bill Division

---
+++ Migratory Birds/solution.cs
@@ -0,0 +1,60 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'migratoryBirds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int migratoryBirds(List<int> arr)
+    {
+       var duplicates = arr
+            .GroupBy(x => x)            
+            .Select(g => new { Number = g.Key,     Count = g.Count() })
+            .Where(g => g.Count > 1);
+            
+            int result = 0;
+            
+            var maxCount = duplicates.Max(x => x.Count);  
+             result = duplicates
+            .Where(x => x.Count == maxCount)
+            .ToList().Min(x=>x.Number);
+            return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.migratoryBirds(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
