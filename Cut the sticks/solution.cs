aeda0a7e874d37dd439868a47196b4cda579a43e
SalmaaSaamir <salmasaamir554@gmail.com>
2025-10-13 15:27:37 +0300

    solve Library Fine && Cut the sticks

---
+++ Cut the sticks/solution.cs
@@ -0,0 +1,59 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'cutTheSticks' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<int> cutTheSticks(List<int> arr)
+    {
+        List<int>  res =  new List<int>{};
+        while (arr.Count > 0)
+         {
+        res.Add(arr.Count); 
+        int min = arr.Min();
+        arr = arr.Select(x => x - min)
+                 .Where(x => x > 0)
+                 .ToList();
+            
+        }
+        return res;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> result = Result.cutTheSticks(arr);
+
+        textWriter.WriteLine(String.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
