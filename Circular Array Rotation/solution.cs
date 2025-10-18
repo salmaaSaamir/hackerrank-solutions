051f200c9266af401d0caf996b2c3472ec7a5522
SalmaaSaamir <salmasaamir554@gmail.com>
2025-10-18 17:09:30 +0300

    solve Circular Array Rotation

---
+++ Circular Array Rotation/solution.cs
@@ -0,0 +1,62 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static List<int> circularArrayRotation(List<int> a, int k, List<int> queries)
+    {
+        int n = a.Count;
+        k = k % n;
+        List<int> result = new List<int>();
+
+        foreach (int q in queries)
+        {
+            int index = (q - k + n) % n;
+            result.Add(a[index]);
+        }
+
+        return result;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+        int q = Convert.ToInt32(firstMultipleInput[2]);
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        List<int> queries = new List<int>();
+
+        for (int i = 0; i < q; i++)
+        {
+            int queriesItem = Convert.ToInt32(Console.ReadLine().Trim());
+            queries.Add(queriesItem);
+        }
+
+        List<int> result = Result.circularArrayRotation(a, k, queries);
+
+        textWriter.WriteLine(String.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
